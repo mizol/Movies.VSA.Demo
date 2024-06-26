5bb2907211fe79d2836b43ca40cdc29758632949
omizyurko <omizyurko@gmail.com>
2024-06-26 17:24:20 +0300

    Added GlobalEceptionHandler

---
+++ src/Movies.Api/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Movies.Api.Middleware
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, "An unhandled exception has occurred.");
+
+            var response = new ProblemDetails
+            {
+                Type = "Internal server error",
+                Status = 500,
+                Title = "Internal server error",
+                Detail = "An unexpected error occurred. Please try again later."
+            };
+
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            await httpContext.Response.WriteAsJsonAsync(response);
+
+            return true;
+        }
+    }
+}
--- src/Movies.Api/Program.cs
+++ src/Movies.Api/Program.cs
@@ -5,10 +5,12 @@ using Microsoft.EntityFrameworkCore;
 using Movies.Api.Behaviors;
 using Movies.Api.Data;
 using Movies.Api.Endpoints;
+using Movies.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddProblemDetails();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -19,15 +21,20 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
 builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
+// Register global exception handler
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.UseDeveloperExceptionPage();    
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
 
 // Map the default endpoint
 app.MapGet("/", () => "Welcome to the Movies API! \r\n\r\nIt's a pet project to learn minimal-api and Verstical Slice Architecture (VSA).");
